4ccd16d9a6c8d887eef11f9dd569aefdf98e6952
Kinga Kohari <kinguca@yahoo.com>
2022-05-17 19:45:27 +0200

    OK

--- src/Page.js
+++ src/Page.js
@@ -4,23 +4,25 @@ import {useContext} from 'react';
 import styled from 'styled-components';
 
 const Button = styled.button`
-    font-size: 20px;
-    padding: 10px 20px;
-    cursor: pointer;
-    color: ${props => props.primary ? 'blue' : 'orange'};
-    background-color: ${props => props.primary ? 'orange' : 'blue'};
+  font-size: 20px;
+  padding: 10px 20px;
+  cursor: pointer;
+  color: ${props => props.primary ? 'blue' : 'orange'};
+  background-color: ${props => props.primary ? 'orange' : 'blue'};
 `
 
-
 function Page() {
+
     const {theme, toggleTheme} = useContext(ThemeContext)
+
   return (
+
     <div className={theme}>
-        {
-            theme === 'light' ?
-            <Button onClick={toggleTheme}>Change Theme</Button> :
-            <Button primary onClick={toggleTheme}>Change Theme</Button>
-        }
+      {
+        theme === 'light' ?
+        <Button onClick={toggleTheme}>Change Theme</Button> :
+        <Button primary onClick={toggleTheme}>Change Theme</Button>
+      }
     </div>
   )
 }
